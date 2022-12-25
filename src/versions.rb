874c1a3511b4760f2da3c71a553047063706cdb6
3colorr <3colorr@gmail.com>
2022-12-25 22:46:36 +0900

    Changed a version.

--- src/versions.rb
+++ src/versions.rb
@@ -1,6 +1,6 @@
 module Versions
     def show_version
-        puts "recent-projects -> version: 0.0.1"
+        puts "recent-projects -> version: 1.0.0"
         puts "Copyright (c) 2022 3colorr"
         puts "MIT License"
     end
